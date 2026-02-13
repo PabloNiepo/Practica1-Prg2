5119361f692f92bd6595cf4d9f6803c3657e63b8
pniep <pniep@DESKTOP-CKC039L>
2026-02-13 02:59:24 -0300

    formulario

--- Practica1/Form1.Designer.cs
+++ Practica1/Form1.Designer.cs
@@ -28,13 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbDestino = new System.Windows.Forms.ComboBox();
+            this.cbTransporte = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.btnResumen = new System.Windows.Forms.Button();
+            this.groupBoxPjero = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCuil = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.groupBoxPjero.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Destino:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Precio:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Transporte:";
+            // 
+            // cbDestino
+            // 
+            this.cbDestino.FormattingEnabled = true;
+            this.cbDestino.Location = new System.Drawing.Point(101, 20);
+            this.cbDestino.Name = "cbDestino";
+            this.cbDestino.Size = new System.Drawing.Size(121, 21);
+            this.cbDestino.TabIndex = 3;
+            // 
+            // cbTransporte
+            // 
+            this.cbTransporte.FormattingEnabled = true;
+            this.cbTransporte.Location = new System.Drawing.Point(101, 74);
+            this.cbTransporte.Name = "cbTransporte";
+            this.cbTransporte.Size = new System.Drawing.Size(121, 21);
+            this.cbTransporte.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(277, 18);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(98, 23);
+            this.btnGen.TabIndex = 6;
+            this.btnGen.Text = "Generar";
+            this.btnGen.UseVisualStyleBackColor = true;
+            // 
+            // btnResumen
+            // 
+            this.btnResumen.Location = new System.Drawing.Point(277, 71);
+            this.btnResumen.Name = "btnResumen";
+            this.btnResumen.Size = new System.Drawing.Size(98, 23);
+            this.btnResumen.TabIndex = 7;
+            this.btnResumen.Text = "Resumen";
+            this.btnResumen.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxPjero
+            // 
+            this.groupBoxPjero.Controls.Add(this.txtTel);
+            this.groupBoxPjero.Controls.Add(this.txtNombre);
+            this.groupBoxPjero.Controls.Add(this.txtCuil);
+            this.groupBoxPjero.Controls.Add(this.label7);
+            this.groupBoxPjero.Controls.Add(this.label6);
+            this.groupBoxPjero.Controls.Add(this.label5);
+            this.groupBoxPjero.Controls.Add(this.label4);
+            this.groupBoxPjero.Location = new System.Drawing.Point(23, 112);
+            this.groupBoxPjero.Name = "groupBoxPjero";
+            this.groupBoxPjero.Size = new System.Drawing.Size(352, 201);
+            this.groupBoxPjero.TabIndex = 8;
+            this.groupBoxPjero.TabStop = false;
+            this.groupBoxPjero.Text = "Datos pasajero:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Cuit/Cuil";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Nombre:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Teléfono:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "N° Tarjeta:";
+            // 
+            // txtCuil
+            // 
+            this.txtCuil.Location = new System.Drawing.Point(123, 36);
+            this.txtCuil.Name = "txtCuil";
+            this.txtCuil.Size = new System.Drawing.Size(100, 20);
+            this.txtCuil.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(123, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(126, 90);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(100, 20);
+            this.txtTel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(627, 396);
+            this.Controls.Add(this.groupBoxPjero);
+            this.Controls.Add(this.btnResumen);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbTransporte);
+            this.Controls.Add(this.cbDestino);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBoxPjero.ResumeLayout(false);
+            this.groupBoxPjero.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbDestino;
+        private System.Windows.Forms.ComboBox cbTransporte;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Button btnResumen;
+        private System.Windows.Forms.GroupBox groupBoxPjero;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCuil;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTel;
     }
 }
 
