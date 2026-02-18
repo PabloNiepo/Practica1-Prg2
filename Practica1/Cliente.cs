e7e3153f9f6a1a5d586e2e8e32e30d88a1c95e43
pniep <pniep@DESKTOP-CKC039L>
2026-02-18 18:21:21 -0300

    v0.0.1

---
+++ Practica1/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class Cliente
+    {
+        private string nombre;
+        private string CUIT;
+        private string telefono;
+        private string tc;
+
+        public Cliente(string nombre, string cuit, string telefono, string tc)
+        {
+            this.nombre = nombre;
+            this.CUIT = cuit;
+            this.telefono = telefono;
+            this.tc = tc;
+        }
+
+        public override string ToString()
+        {
+            return @"
+                     ---------------------------------
+                      Agencia de viajes Final PRG II
+                     ---------------------------------
+             ";
+        }
+    }
+}
---
+++ Practica1/Sistema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    [Serializable]
+    internal class Sistema
+    {
+        private List<Ticket> tickets;
+
+        public List<Ticket> Tickets { get { return tickets; } }
+
+        public void GeneroTicket() { }
+        
+    }
+}
